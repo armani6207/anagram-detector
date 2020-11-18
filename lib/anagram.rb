36738d5fc738c0435c9c19717f7da2f4f18f586d
Armando <rojas.armani@gmail.com>
2020-11-18 16:58:15 -0500

    commit tree d01400c7328db4ba8216300b4c9543b93bb47708

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        ret_array = []
+        array.each do |anagram|
+            mod_word = @word.chars.sort
+            mod_anagram = anagram.chars.sort
+            if mod_word == mod_anagram
+                ret_array << anagram
+            end
+        end
+        ret_array
+    end
+
+
+end
